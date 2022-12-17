8d4be8b9a7108108614a161c5dd1cf6f2cbc9e31
Rauane Lima <rauanelima7@gmail.com>
2022-12-17 17:24:13 -0300

    estilizando NavBar

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import NavBar from "./Navbar";
 
 function MainContent() {
-  return <div />;
+  return (
+    <Container>
+      <NavBar />
+      <SubContainer>
+        <SectionOne>
+          <ColumOnel1></ColumOnel1>
+          <ColumTwol1></ColumTwol1>
+        </SectionOne>
+        <SectionTwor>
+          <ColumnOne2></ColumnOne2>
+          <ColumnTwol2></ColumnTwol2>
+        </SectionTwor>
+      </SubContainer>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 80%;
+  margin: 1rem 8rem 1rem 4rem;
+`;
+const SubContainer = styled.div`
+  margin: 0.5rem 0;
+  height: 80%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+`;
+const TitleText = styled.h3`
+  height: 20%;
+`;
+const SectionOne = styled.div``;
+const SectionTwor = styled.div``;
+const ColumOnel1 = styled.div``;
+const ColumnOne2 = styled.div``;
+const ColumTwol1 = styled.div``;
+const ColumnTwol2 = styled.div``;
 
 export default MainContent;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import styled from "styled-components";
+import { FiSearch } from "react-icons/fi";
+import { BsPlusLg } from "react-icons/bs";
+
+function NavBar() {
+  return (
+    <NavContainer>
+      <Text>
+        Good Night, <br />
+        <span>Rauane Lima</span>
+      </Text>
+      <InputContainer>
+        <Icon>
+          <FiSearch />
+        </Icon>
+        <Input type="text" placeholder="Search for projects" />
+      </InputContainer>
+
+      <MakeContainer>
+        <TextMore>Make a new Product</TextMore>
+        <IconMore>
+          <BsPlusLg />
+        </IconMore>
+      </MakeContainer>
+    </NavContainer>
+  );
+}
+
+const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 10%;
+`;
+const Text = styled.h1`
+  color: #ffffff;
+  span {
+    font-size: 1.5rem;
+    font-weight: 400;
+    color: #16b88f;
+  }
+`;
+const InputContainer = styled.div`
+  display: flex;
+`;
+const Icon = styled.div`
+  height: 4.5rem;
+  width: 3.5rem;
+  background-color: #088171;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-top-left-radius: 0.5rem;
+  border-bottom-left-radius: 0.5rem;
+  svg {
+    color: #0d523b;
+  }
+`;
+const MakeContainer = styled.button`
+  background-color: #363e52;
+  cursor: pointer;
+  border-radius: 10px;
+  border: none;
+  gap: 5.83px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  width: 231.58px;
+height: 66.04px;
+`;
+const IconMore = styled.div`
+display: flex;
+background-color: #0f6369;
+padding: 10px;
+align-items: center;
+  color: white;
+`;
+const TextMore = styled.p`
+color: white;
+font-size: 1rem;
+font-weight: 500;
+`
+const Input = styled.input`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 5.83px;
+  border: none;
+  width: 400px;
+  height: 70px;
+
+  background: #2e323c;
+  border-bottom-right-radius: 11.654px;
+  border-top-right-radius: 11.654px;
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+export default NavBar;
